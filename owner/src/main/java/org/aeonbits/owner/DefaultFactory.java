1f6ce72414bf850f4d7a5e7d5e6d655a9f053bb1
regbo <reggie.pierce@iplasso.com>
2020-04-21 06:41:19 -0400

    syncing with upstream

--- owner/src/main/java/org/aeonbits/owner/DefaultFactory.java
+++ owner/src/main/java/org/aeonbits/owner/DefaultFactory.java
@@ -8,15 +8,20 @@
 
 package org.aeonbits.owner;
 
-import org.aeonbits.owner.loaders.Loader;
-
-import javax.management.DynamicMBean;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ScheduledExecutorService;
-
 import static java.lang.reflect.Proxy.newProxyInstance;
 import static org.aeonbits.owner.util.Reflection.isClassAvailable;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+
+import javax.management.DynamicMBean;
+
+import org.aeonbits.owner.lfp.LFPUtils;
+import org.aeonbits.owner.loaders.Loader;
 
 /**
  * Default implementation for {@link Factory}.
@@ -25,86 +30,89 @@ import static org.aeonbits.owner.util.Reflection.isClassAvailable;
  */
 class DefaultFactory implements Factory {
 
-    private static final boolean isJMXAvailable = isClassAvailable("javax.management.DynamicMBean");
-    private final ScheduledExecutorService scheduler;
-    private Properties props;
-    final LoadersManager loadersManager;
+	private static final boolean isJMXAvailable = isClassAvailable("javax.management.DynamicMBean");
+	private final ScheduledExecutorService scheduler;
+	private Properties props;
+	final LoadersManager loadersManager;
 
-    DefaultFactory(ScheduledExecutorService scheduler, Properties props) {
-        this.scheduler = scheduler;
-        this.props = props;
-        this.loadersManager = new LoadersManager();
-    }
+	DefaultFactory(ScheduledExecutorService scheduler, Properties props) {
+		this.scheduler = scheduler;
+		this.props = props;
+		this.loadersManager = new LoadersManager();
+	}
 
-    @SuppressWarnings("unchecked")
-    public <T extends Config> T create(Class<? extends T> clazz, Map<?, ?>... imports) {
-        Class<?>[] interfaces = interfaces(clazz);
-        VariablesExpander expander = new VariablesExpander(props);
-        PropertiesManager manager = new PropertiesManager(clazz, new Properties(), scheduler, expander, loadersManager,
-                imports);
-        Object jmxSupport = getJMXSupport(clazz, manager);
-        PropertiesInvocationHandler handler = new PropertiesInvocationHandler(manager, jmxSupport);
-        T proxy = (T) newProxyInstance(clazz.getClassLoader(), interfaces, handler);
-        handler.setProxy(proxy);
-        return proxy;
-    }
+	@SuppressWarnings("unchecked")
+	public <T extends Config> T create(Class<? extends T> clazz, Map<?, ?>... imports) {
+		Class<?>[] interfaces = interfaces(clazz);
+		VariablesExpander expander = new VariablesExpander(props);
+		PropertiesManager manager = new PropertiesManager(clazz, new Properties(), scheduler, expander, loadersManager,
+				imports);
+		Object jmxSupport = getJMXSupport(clazz, manager);
+		PropertiesInvocationHandler handler = new PropertiesInvocationHandler(manager, jmxSupport);
+		T proxy = (T) newProxyInstance(clazz.getClassLoader(), interfaces, handler);
+		handler.setProxy(proxy);
+		return proxy;
+	}
 
-    public String setProperty(String key, String value) {
-        checkKey(key);
-        return (String) props.setProperty(key, value);
-    }
+	public String setProperty(String key, String value) {
+		checkKey(key);
+		return (String) props.setProperty(key, value);
+	}
 
-    private void checkKey(String key) {
-        if (key == null)
-            throw new IllegalArgumentException("key can't be null");
-        if (key.isEmpty())
-            throw new IllegalArgumentException("key can't be empty");
-    }
+	private void checkKey(String key) {
+		if (key == null)
+			throw new IllegalArgumentException("key can't be null");
+		if (key.isEmpty())
+			throw new IllegalArgumentException("key can't be empty");
+	}
 
-    public Properties getProperties() {
-        return props;
-    }
+	public Properties getProperties() {
+		return props;
+	}
 
-    public void setProperties(Properties properties) {
-        if (properties == null)
-            props = new Properties();
-        else
-            props = properties;
-    }
+	public void setProperties(Properties properties) {
+		if (properties == null)
+			props = new Properties();
+		else
+			props = properties;
+	}
 
-    public void registerLoader(Loader loader) {
-        loadersManager.registerLoader(loader);
-    }
+	public void registerLoader(Loader loader) {
+		loadersManager.registerLoader(loader);
+	}
 
-    public void setTypeConverter(Class<?> type, Class<? extends Converter<?>> converter) {
-        Converters.setTypeConverter(type, converter);
-    }
+	public void setTypeConverter(Class<?> type, Class<? extends Converter<?>> converter) {
+		Converters.setTypeConverter(type, converter);
+	}
 
-    public void removeTypeConverter(Class<?> type){
-        Converters.removeTypeConverter(type);
-    }
+	public void removeTypeConverter(Class<?> type) {
+		Converters.removeTypeConverter(type);
+	}
 
-    public String getProperty(String key) {
-        checkKey(key);
-        return props.getProperty(key);
-    }
+	public String getProperty(String key) {
+		checkKey(key);
+		return props.getProperty(key);
+	}
 
-    public String clearProperty(String key) {
-        checkKey(key);
-        return (String) props.remove(key);
-    }
+	public String clearProperty(String key) {
+		checkKey(key);
+		return (String) props.remove(key);
+	}
 
-    private Object getJMXSupport(Class<?> clazz, PropertiesManager manager) {
-        if (isJMXAvailable)
-            return new JMXSupport(clazz, manager);
-        return null;
-    }
+	private Object getJMXSupport(Class<?> clazz, PropertiesManager manager) {
+		if (isJMXAvailable)
+			return new JMXSupport(clazz, manager);
+		return null;
+	}
 
-    private <T extends Config> Class<?>[] interfaces(Class<? extends T> clazz) {
-        if (isJMXAvailable)
-            return new Class<?>[]{clazz, DynamicMBean.class};
-        else
-            return new Class<?>[]{clazz};
-    }
+	private <T extends Config> Class<?>[] interfaces(Class<? extends T> clazz) {
+		Set<Class<?>> ifaces = new LinkedHashSet<Class<?>>(Arrays.asList(LFPUtils.getInterfaces(clazz)));
+
+		if (isJMXAvailable)
+			ifaces.addAll(Arrays.asList(new Class<?>[] { clazz, DynamicMBean.class }));
+		else
+			ifaces.addAll(Arrays.asList(new Class<?>[] { clazz }));
+		return ifaces.toArray(new Class[ifaces.size()]);
+	}
 
 }
